fc4fbe73c9e0a0f512f4635e104a93278fc38d7c
Gustavo Coelho Serezini <serezinigustavo@gmail.com>
2025-06-17 10:09:32 -0300

    add input validation

--- programa.c
+++ programa.c
@@ -1,6 +1,4 @@
 #include <locale.h>
-<<<<<<< Updated upstream
-=======
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -14,7 +12,6 @@ int Verificarlogin();
 int Login();
 void Menu();
 int cadastrarItem();
->>>>>>> Stashed changes
 
 //estrutura global
 struct produto{
@@ -26,27 +23,19 @@ struct produto{
 struct produto produtos[100];
 int contador = 0;
 
-<<<<<<< Updated upstream
-
-int main(){
-=======
 void main(){
 	setlocale(LC_ALL, "Portuguese");
 	if(login!=1){
 		nivelacesso=Login();
 	}
->>>>>>> Stashed changes
 	
-	setlocale(LC_ALL,"Portuguese");
-	
-	//teste
-	return 0;
+	if(nivelacesso!=0 && login==1){
+		//chamar menu e nele analisar o nivel de acesso
+		sleep(1);
+		Menu();
+	}
 }
 
-<<<<<<< Updated upstream
-//teste
-//teste
-=======
 
 void Menu(){
 	int op;
@@ -185,52 +174,107 @@ int Verificarlogin() {
 
 int cadastrarItem(){
 	
-	char temp;
-
+    char temp;
+    int codigoTemp, qtdTemp, i;
+    float precoTemp;
+    
     if(contador >= 100){
         printf("Limite de produtos atingido!\n");
         return 0;
-    } 
-	
-    printf("Digite o nome do produto: ");
-    while (getchar() != '\n'); // Limpa o buffer
-    fgets(produtos[contador].nome, sizeof(produtos[contador].nome), stdin);
-    produtos[contador].nome[strcspn(produtos[contador].nome, "\n")] = '\0';
-    
-    printf("Digite o código do produto: ");
-    scanf("%d",&produtos[contador].codigo);
-    
-    printf("Digite o preço do produto: ");
-    scanf("%f",&produtos[contador].preco);
-    
-    printf("Digite a quantidade do produto: ");
-    scanf("%d",&produtos[contador].qtd);
-    
-    printf("Ele é genérico?[s/n]: ");
-    while (getchar() != '\n'); 
-    scanf("%c",&temp);
-    
-    if(temp == 's' || temp == 'S'){
-        produtos[contador].generico = true;
-    } else {
-        produtos[contador].generico = false;
     }
     
-    contador++; 
-    printf("Produto cadastrado com sucesso!\n");
+
+    // Validação do nome do produto
+    do {
+    	while (getchar() != '\n'); 
+        printf("Digite o nome do produto: ");
+        fgets(produtos[contador].nome, sizeof(produtos[contador].nome), stdin);
+        produtos[contador].nome[strcspn(produtos[contador].nome, "\n")] = '\0';
+        
+        if(strlen(produtos[contador].nome) == 0){
+            printf("Erro: Nome do produto não pode estar vazio! Tente novamente.\n");
+        } else {
+            break; 
+        }
+    } while(1);
     
+    // Validação do código do produto
+    do {
+        printf("Digite o código do produto (número positivo): ");
+        if(scanf("%d", &codigoTemp) != 1) {
+            printf("Erro: Digite apenas números!\n");
+            while (getchar() != '\n'); 
+            codigoTemp = -1; //caso o scanf falhe, codigoTemp valerá um valor anterior, então aqui ele é declarado como -1 pra forçar o erro e ser lido novamente
+        } else if(codigoTemp <= 0) {
+            printf("Erro: Código deve ser um número positivo!\n");
+        } else {
+            // Verifica se o código já existe
+            int codigoExiste = 0;
+            for(i = 0; i < contador; i++){
+                if(produtos[i].codigo == codigoTemp){
+                    printf("Erro: Este código já existe! Digite outro código.\n");
+                    codigoExiste = 1;
+                    break;
+                }
+            }
+            if(!codigoExiste) {
+                produtos[contador].codigo = codigoTemp;
+                break;
+            }
+        }
+    } while(1);
+    
+    // Validação do preço
+    do {
+        printf("Digite o preço do produto (valor positivo): R$ ");
+        if(scanf("%f", &precoTemp) != 1) {
+            printf("Erro: Digite um número válido!\n");
+            while (getchar() != '\n'); 
+        } else if(precoTemp <= 0) {
+            printf("Erro: Preço deve ser maior que zero!\n");
+        } else {
+            produtos[contador].preco = precoTemp;
+            break;
+        }
+    } while(1);
+    
+    // Validação da quantidade
+    do {
+        printf("Digite a quantidade do produto (número não negativo): ");
+        if(scanf("%d", &qtdTemp) != 1) {
+            printf("Erro: Digite apenas números!\n");
+            while (getchar() != '\n'); 
+        } else if(qtdTemp < 0) {
+            printf("Erro: Quantidade não pode ser negativa!\n");
+        } else {
+            produtos[contador].qtd = qtdTemp;
+            break;
+        }
+    } while(1);
+    
+    // Validação se é genérico
+    do {
+        printf("Ele é genérico? [s/n]: ");
+        while (getchar() != '\n'); 
+        scanf("%c", &temp);
+        
+        if(temp == 's' || temp == 'S'){
+            produtos[contador].generico = true;
+            break;
+        } else if(temp == 'n' || temp == 'N'){
+            produtos[contador].generico = false;
+            break;
+        } else {
+            printf("Erro: Digite apenas 's' para sim ou 'n' para não!\n");
+        }
+    } while(1);
+    
+    contador++; 
+    
+    printf("\nProduto cadastrado com sucesso!\n");
+	system("pause");
+	system("cls");
     return 1;
 }
 
 
-
-
-
-
-
-
-
-
-
-
->>>>>>> Stashed changes
